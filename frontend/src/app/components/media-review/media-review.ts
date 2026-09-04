eed8532008ec0d6623f1012b4f1d9c9331bb2a9a
yogurtshi <yoshi.gyger@bbzbl-it.ch>
2026-09-04 09:31:21 +0200

    feat: menu for comments and small styling changes

--- frontend/src/app/components/media-review/media-review.ts
+++ frontend/src/app/components/media-review/media-review.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIcon } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
 import { Comment } from '../../data/comment';
 import { IsInRolesDirective } from '../../directives/app-is-in-roles.dir';
 
 @Component({
   selector: 'app-media-review',
-  imports: [MatButtonModule, IsInRolesDirective],
+  imports: [MatButtonModule, MatIcon, MatMenuModule, IsInRolesDirective],
   templateUrl: './media-review.html',
   styleUrl: './media-review.scss',
 })
